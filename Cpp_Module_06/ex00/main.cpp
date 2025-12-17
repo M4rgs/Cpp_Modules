51f4e83f316d600e3d098d1aee856348adc884c1
Taha Mounir <tamounir@e1r10p12.1337.ma>
2025-12-17 17:11:27 +0100

    added av char pointer to std::string class

--- Cpp_Module_06/ex00/main.cpp
+++ Cpp_Module_06/ex00/main.cpp
@@ -1,7 +1,11 @@
 #include "Scalar.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-    ScalarConverter pp;
-    pp.convert("97");
+    if (ac != 2)
+    {
+        std::cout << "Please run it as ./Scalar <args>" << std::endl;
+        return 1;
+    }
+    ScalarConverter::convert(std::string(av[1]));
 }
