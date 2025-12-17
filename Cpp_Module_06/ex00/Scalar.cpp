19cc81cbb089dfa9e7b0bfab4b9db4056b542632
Taha Mounir <tamounir@e1r2p13.1337.ma>
2025-12-17 18:42:21 +0100

    adding isint checker

--- Cpp_Module_06/ex00/Scalar.cpp
+++ Cpp_Module_06/ex00/Scalar.cpp
@@ -16,6 +16,29 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter& other)
     return (*this);
 }
 
+void    pritnResult(int inte, double dbl, float flt, char chr)
+{
+    std::cout << "as int : " << inte << std::endl;
+    std::cout << "as double : " << dbl << std::endl;
+    std::cout << "as float : " << flt << "f" << std::endl;
+    std::cout << "as char : " << chr << std::endl;
+}
+
+bool isInt(const std::string input)
+{
+    size_t i = 0;
+    if (input[0] == '+' || input[0] == '-')
+        i = 1;
+    if (i && !input[1])
+        return (false);
+    for (; i < input.size(); i++)
+    {
+        if (input[i] < '0' || input[i] > '9')
+            return (false);
+    }
+    return (true);
+}
+
 void ScalarConverter::convert(const std::string input)
 {
 
@@ -24,15 +47,23 @@ void ScalarConverter::convert(const std::string input)
     float flt = 0;
     char chr = 0;
 
-    int bb = atoi(input.c_str());
-    inte = bb;
-    dbl = static_cast<char>(bb);
-    flt = static_cast<double>(bb);
-    chr = static_cast<char>(bb);
+    if (isInt(input) == true)
+    {
+        inte = atoi(input.c_str());
+        dbl = static_cast<double>(inte);
+        flt = static_cast<float>(inte);
+        chr = static_cast<char>(inte);
+    }
+    else
+    {
+        std::cout << "ERROR NOT INT !" << std::endl;
+        exit (1);
+    }
+    pritnResult(inte, dbl, flt, chr);
 
-    std::cout << "as int : " << inte << "\n" << "as double : " << dbl << "\n" << "as float : " << flt << "\n" << "as char : " << chr << std::endl;
 }
 
 ScalarConverter::~ScalarConverter()
 {
 }
+
