8ce5e8965a6ff7654366db78b0202320456c3c35
Taha Mounir <tamounir@e1r10p12.1337.ma>
2026-02-25 02:07:12 +0000

    finished 07

--- Cpp_Module_07/ex02/Array.hpp
+++ Cpp_Module_07/ex02/Array.hpp
@@ -2,7 +2,77 @@
 #define ARRAY_HPP
 
 #include <iostream>
-#include <array>
 
+template <typename T>
 
+class Array {
+
+    private :
+        T *arr;
+        unsigned int arrSize;
+
+    public :
+        Array()
+        {
+            this->arrSize = 0;
+            this->arr = NULL;
+        }
+
+        Array(unsigned int n)
+        {
+            this->arrSize = n;
+            this->arr = new T[this->arrSize];
+        }
+
+        Array(const Array &other)
+        {
+            arrSize = other.arrSize;
+            arr = NULL;
+            *this = other;
+        }
+
+        Array &operator=(const Array &other)
+        {
+            if (this == &other)
+                return *this;
+
+            delete[] arr;
+            arr = NULL;
+
+            arrSize = other.arrSize;
+
+            if (arrSize > 0)
+            {
+                arr = new T[arrSize];
+                for (unsigned int i = 0; i < arrSize; i++)
+                    arr[i] = other.arr[i];
+            }
+
+            return *this;
+        }
+        class OutOfBound : public std::exception
+        {
+            public:
+                const char *what() const throw()
+                {
+                    return ("Index out of bound !");
+                }
+        };
+
+        T& operator[](unsigned int index)
+        {
+            if (index >= this->arrSize)
+                throw(OutOfBound());
+            return (this->arr[index]);
+        }
+        unsigned int size() const
+        {
+            return (this->arrSize);
+        }
+
+        ~Array()
+        {
+            delete [] arr;
+        }
+};
 #endif
